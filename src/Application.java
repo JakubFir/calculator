7f2625401a154f224572b95bf0f53394ed3d007d
Jakub <jakubfirlejczyk@gmail.com>
2022-11-24 15:57:37 +0100

    first commit

---
+++ src/Application.java
@@ -0,0 +1,15 @@
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
+class Calculator{
+    public int addition (int firstNumber, int secondNumber){
+        int result = firstNumber + secondNumber;
+        return result;
+    }
+    public int subtraction (int firstNumber, int secondNumber){
+        int result = firstNumber - secondNumber;
+        return result;
+    }
+}
